d562d979fab7f175c4dbf65ff84970d0ae6baa9f
rahulswain85 <rahul8033swain@gmail.com>
2025-10-26 20:21:37 +0530

    react-router

---
+++ react-router-demo/src/main.jsx
@@ -0,0 +1,51 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from "./components/About/About.jsx";
+import ContactUs from "./components/ContactUs/ContactUs.jsx";
+import User from "./components/User/User.jsx";
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+
+//       {
+//         path: "About",
+//         element: <About />,
+//       },
+
+//       {
+//         path: "ContactUs",
+//         element: <ContactUs />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/ContactUs" element={<ContactUs />} />
+      <Route path="/User/:userId" element={<User />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
